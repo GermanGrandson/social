24920d8887e00f52a6378b133ba44396f8cab1fe
German Grandson <ganieto1@gmail.com>
2015-12-19 19:38:57 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/new'
-
   root 'static_pages#home'
+  resources :users
+
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
